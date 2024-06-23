8b52e9e2ee9060a8fa2d80b22d6b275dd5a80307
sacsangade <sacsangade@gmail.com>
2024-06-24 00:07:21 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -94,7 +94,7 @@ function Home() {
         </div>
       </section>
 
-      <section className="section2  w-full max-w-screen-2xl px-5 sm:px-8 mx-auto sm:py-16">
+      <section className="section2  w-full max-w-screen-2xl px-5 sm:px-8 mx-auto py-16">
         <div className="flex flex-col items-center text-center md:py-32">
           <h3 className="max-w-2xl">
             <span className="font-medium ml-2 dark:text-white">21,450</span>
