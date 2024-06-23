11cf22eda62328b8c4d67019d6f98458d906e5b7
sacsangade <sacsangade@gmail.com>
2024-06-23 21:51:53 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -34,7 +34,7 @@ function Header() {
       <a href='/'>
         <h3 className='font-bold dark:text-white'>HHU</h3>
       </a>
-        <div className="menu-btn"></div>
+        <div className="menu-btn md:hidden"></div>
         </div>
       <div className="hidden md:flex items-center gap-16 dark:text-white">
         <a href='/#about-us' className="text2 cursor-pointer">About Us</a>
