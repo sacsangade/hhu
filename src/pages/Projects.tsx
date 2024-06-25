85bf6247874fad5f2ba17efa3eed0743724748c1
sacsangade <sacsangade@gmail.com>
2024-06-26 03:09:49 +0530

    Update Projects.tsx

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -29,7 +29,7 @@ function Projects() {
         <h2 className="text-[30px] sm:text-[48px] leading-[1.5]">
           How You Can Help
         </h2>
-        <div className="mt-4 sm:mt-12 flex gap-20">
+        <div className="mt-4 sm:mt-12 flex flex-col md:flex-row gap-10 md:gap-20">
           <div className="">
             <p className="text1 font-['Roboto-thin'] dark:text-[#909090]">
               t vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
