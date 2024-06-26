5b0438317c6932fca47c48a5487bbf25aeb77834
sacsangade <sacsangade@gmail.com>
2024-06-27 01:17:02 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -51,7 +51,6 @@ const events = [
 ];
 
 function Home() {
-  const form = useRef();
   const [message, setMessage] = useState('');
   const [sname, setSname] = useState('');
    const [sepvalue, setSepvalue] = useState('');
@@ -87,17 +86,11 @@ function Home() {
    
 const handleform = (e: React.FormEvent<HTMLFormElement>) =>{
   e.preventDefault();
-  const currentForm = form.current;
-   if (currentForm == null) return;
+
   
   if(sname && sepvalue && smessage)
   {
-     emailjs.sendForm('service_ws21xtl', 'template_mcvpuol', currentForm, '-Rm081AKzibsOHEEV')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
+    
   }
   else
   {
